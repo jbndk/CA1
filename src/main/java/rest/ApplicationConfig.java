c25a2b236e0c5d94187ad9a1fb60bd294f61cba0
Jonas <j.b-n@live.dk>
2020-09-16 13:02:03 +0200

    Joke, JokeDTO, JokeFacade and JokeResource added

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -21,6 +21,7 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
+        resources.add(rest.JokeResource.class);
         resources.add(rest.MemberResource.class);
         resources.add(rest.TestResource.class);
     }
