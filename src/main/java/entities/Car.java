0a98c472226c80f962da40014028c253aa56bb09
Claes <claesvonh@gmail.com>
2020-09-17 18:21:45 +0200

    non-DTO attributes added to entities.Car

--- src/main/java/entities/Car.java
+++ src/main/java/entities/Car.java
@@ -1,15 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entities;
 
 import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
 
 /**
  *
@@ -23,9 +21,15 @@ public class Car implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private int year;
+    @Column
     private String make;
     private String model;
     private int price;
+
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date created;
+
+    private String createdByUser;
 
     public Car() {
     }
@@ -36,6 +40,9 @@ public class Car implements Serializable {
         this.make = make;
         this.model = model;
         this.price = price;
+
+        this.createdByUser = "Claes Lindholm";
+        this.created = new Date();
 
     }
 
